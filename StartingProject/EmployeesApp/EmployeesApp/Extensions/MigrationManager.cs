cda2dc427c1f860205d03bca5dd3256efb500867
Marinko <marinkospasojevic@gmail.com>
2019-09-15 18:35:54 +0200

    Added starting project in the master branch.

---
+++ StartingProject/EmployeesApp/EmployeesApp/Extensions/MigrationManager.cs
@@ -0,0 +1,35 @@
+﻿using EmployeesApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Extensions
+{
+    public static class MigrationManager
+    {
+        public static IWebHost MigrateDatabase(this IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<EmployeeContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log errors or do anything you think it's needed
+                        throw;
+                    }
+                }
+            }
+
+            return webHost;
+        }
+    }
+}
