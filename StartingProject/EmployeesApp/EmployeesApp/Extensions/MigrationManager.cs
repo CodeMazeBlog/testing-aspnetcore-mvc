469015caf7a8751e19882830bd4f8fe647c27ddf
Marinko <marinkospasojevic@gmail.com>
2020-01-02 16:07:02 +0100

    3.1 support

--- StartingProject/EmployeesApp/EmployeesApp/Extensions/MigrationManager.cs
+++ StartingProject/EmployeesApp/EmployeesApp/Extensions/MigrationManager.cs
@@ -2,16 +2,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EmployeesApp.Extensions
 {
     public static class MigrationManager
     {
-        public static IWebHost MigrateDatabase(this IWebHost webHost)
+        public static IHost MigrateDatabase(this IHost webHost)
         {
             using (var scope = webHost.Services.CreateScope())
             {
