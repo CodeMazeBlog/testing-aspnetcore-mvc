565d0078d9ae53adb0f78fa96335cdf250fddef7
Marinko <marinkospasojevic@gmail.com>
2020-01-02 16:17:12 +0100

    3.1 support

--- EmployeesApp/EmployeesApp.IntegrationTests/EmployeesControllerIntegrationTests.cs
+++ EmployeesApp/EmployeesApp.IntegrationTests/EmployeesControllerIntegrationTests.cs
@@ -1,32 +1,30 @@
+using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace EmployeesApp.IntegrationTests
 {
-    public class EmployeesControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Startup>>
-    {
-        private readonly HttpClient _client;
-
-        public EmployeesControllerIntegrationTests(TestingWebAppFactory<Startup> factory)
+    public class EmployeesControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Startup>> 
+    { 
+        private readonly HttpClient _client; 
+        public EmployeesControllerIntegrationTests(TestingWebAppFactory<Startup> factory) 
         {
-            _client = factory.CreateClient();
+            _client = factory.CreateClient(); 
         }
 
-        [Fact]
-        public async Task Index_WhenCalled_ReturnsApplicationForm()
-        {
-            var response = await _client.GetAsync("/Employees");
+        [Fact] public async Task Index_WhenCalled_ReturnsApplicationForm() 
+        { 
+            var response = await _client.GetAsync("/Employees"); 
 
-            response.EnsureSuccessStatusCode();
+            response.EnsureSuccessStatusCode(); 
 
-            var responseString = await response.Content.ReadAsStringAsync();
+            var responseString = await response.Content.ReadAsStringAsync(); 
 
             Assert.Contains("Mark", responseString);
-            Assert.Contains("Evelin", responseString);
+            Assert.Contains("Evelin", responseString); 
         }
 
         [Fact]
@@ -45,57 +43,56 @@ namespace EmployeesApp.IntegrationTests
         public async Task Create_SentWrongModel_ReturnsViewWithErrorMessages()
         {
             var initResponse = await _client.GetAsync("/Employees/Create");
-            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse);
-
-            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Employees/Create");
-
-            postRequest.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
-
-            var formModel = new Dictionary<string, string>
-            {
-                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "Name", "New Employee" },
-                { "Age", "25" }
-            };
-
-            postRequest.Content = new FormUrlEncodedContent(formModel);
-
-            var response = await _client.SendAsync(postRequest);
-            response.EnsureSuccessStatusCode();
-
-            var responseString = await response.Content.ReadAsStringAsync();
-
+            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse); 
+            
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Employees/Create"); 
+            
+            postRequest.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString()); 
+            
+            var formModel = new Dictionary<string, string> 
+            { 
+                { 
+                    AntiForgeryTokenExtractor.AntiForgeryFieldName, 
+                    antiForgeryValues.fieldValue 
+                },
+                { "Name", "New Employee" }, 
+                { "Age", "25" } 
+            }; 
+            
+            postRequest.Content = new FormUrlEncodedContent(formModel); 
+            
+            var response = await _client.SendAsync(postRequest); 
+            response.EnsureSuccessStatusCode(); 
+            
+            var responseString = await response.Content.ReadAsStringAsync(); 
             Assert.Contains("Account number is required", responseString);
         }
 
-        [Fact]
-        public async Task Create_WhenPOSTExecuted_ReturnsToIndexView()
+        [Fact] 
+        public async Task Create_WhenPOSTExecuted_ReturnsToIndexView() 
         {
-            var initResponse = await _client.GetAsync("/Employees/Create");
-            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse);
-
-            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Employees/Create");
-
-            postRequest.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString());
-
-            var modelData = new Dictionary<string, string>
-            {
-                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue },
-                { "Name", "New Employee" },
-                { "Age", "25" },
-                { "AccountNumber", "214-5874986532-21" }
-            };
-
+            var initResponse = await _client.GetAsync("/Employees/Create"); 
+            var antiForgeryValues = await AntiForgeryTokenExtractor.ExtractAntiForgeryValues(initResponse); 
+            
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Employees/Create"); 
+            
+            postRequest.Headers.Add("Cookie", new CookieHeaderValue(AntiForgeryTokenExtractor.AntiForgeryCookieName, antiForgeryValues.cookieValue).ToString()); 
+            
+            var modelData = new Dictionary<string, string> 
+            { 
+                { AntiForgeryTokenExtractor.AntiForgeryFieldName, antiForgeryValues.fieldValue }, 
+                { "Name", "New Employee" }, 
+                { "Age", "25" }, 
+                { "AccountNumber", "214-5874986532-21" } 
+            }; 
+            
             postRequest.Content = new FormUrlEncodedContent(modelData);
-
-            var response = await _client.SendAsync(postRequest);
-
-            response.EnsureSuccessStatusCode();
-
-            var responseString = await response.Content.ReadAsStringAsync();
-
-            Assert.Contains("New Employee", responseString);
-            Assert.Contains("214-5874986532-21", responseString);
-        }
+            
+            var response = await _client.SendAsync(postRequest); 
+            response.EnsureSuccessStatusCode(); 
+            
+            var responseString = await response.Content.ReadAsStringAsync(); 
+            
+            Assert.Contains("New Employee", responseString); Assert.Contains("214-5874986532-21", responseString); }
     }
 }
